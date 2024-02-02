adafe5ad37ab5b2cf96d859cc3626adbd14e42f4
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-01 19:56:00 -0500

    python-p3-oop-putting-it-all-together/
    python-p3-oop-putting-it-all-together/

--- python-p3-oop-putting-it-all-together-main/lib/book.py
+++ python-p3-oop-putting-it-all-together-main/lib/book.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self, title, page_count):
+    
+    def __init__(self, title = "And Then There Were None", page_count = 272):
         self.title = title
         self.page_count = page_count
 
-    def get_page_count(self):
+    @property
+    def page_count(self):
         return self._page_count
     
-    def set_page_count(self, page_count):
-        if type(page_count) is int:
+    @page_count.setter
+    def page_count(self, page_count):
+        if type(page_count) == int:
             self._page_count = page_count
         else:
-            print('page_count must be an integer')
-
-    page_count = property(get_page_count, set_page_count)
-
+            print("page_count must be an integer")
+    
     def turn_page(self):
-        print('Flipping the page...wow, you read fast!')
+        print("Flipping the page...wow, you read fast!")
--- python-p3-oop-putting-it-all-together-main/lib/shoe.py
+++ python-p3-oop-putting-it-all-together-main/lib/shoe.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    
     def __init__(self, brand, size):
         self.brand = brand
         self.size = size
 
-    def get_size(self):
-       return self._size 
+    @property
+    def size(self):
+        return self._size
     
-    def set_size(self, size):
-        if type(size) is int:
+    @size.setter
+    def size(self, size):
+        if type(size) == int:
             self._size = size
         else:
-            print('size must be an integer')
-
-    size = property(get_size, set_size)
+            print("size must be an integer")
 
     def cobble(self):
-        print('Your shoe is as good as new!')
-        self.condition = 'New'    
+        self.condition = "New"
+        print("Your shoe is as good as new!")
