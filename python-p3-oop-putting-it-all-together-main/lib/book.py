0dd44a26270596809eeb4a60aee2133ca7e3ed31
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-01 20:07:41 -0500

    python-p3-oop-putting-it-all-together/
    python-p3-oop-putting-it-all-together/

--- python-p3-oop-putting-it-all-together-main/lib/book.py
+++ python-p3-oop-putting-it-all-together-main/lib/book.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self, title, page_count):
+    def __init__(self, title, pages, price):
+        if not isinstance(pages, int):
+            raise ValueError("page_count must be an integer")
         self.title = title
-        self.page_count = page_count
-    pass
-    
+        self.pages = pages
+        self.price = price
+        self.discount = 0
+
+    def set_discount(self, amount):
+        if 0 <= amount <= 100:
+            self.discount = amount
+        else:
+            raise ValueError("Discount must be between 0 and 100.")
+
+    def discounted_price(self):
+        return self.price * (1 - self.discount / 100)
+
+    def turn_page(self):
+        return "Flipping the page...wow, you read fast!"
+
         
