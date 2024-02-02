ee89105e3922fee8539264ae178b1a2629e0e48a
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-01 19:57:14 -0500

    python-p3-oop-putting-it-all-together/
    python-p3-oop-putting-it-all-together/

--- python-p3-oop-putting-it-all-together-main/lib/book.py
+++ python-p3-oop-putting-it-all-together-main/lib/book.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    
-    def __init__(self, title = "And Then There Were None", page_count = 272):
+    def __init__(self, title):
         self.title = title
-        self.page_count = page_count
+        self.author = None
+        self.page_count = None
+        self.genre = None
 
     @property
     def page_count(self):
         return self._page_count
-    
+
     @page_count.setter
-    def page_count(self, page_count):
-        if type(page_count) == int:
-            self._page_count = page_count
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
         else:
             print("page_count must be an integer")
-    
+
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
--- python-p3-oop-putting-it-all-together-main/lib/shoe.py
+++ python-p3-oop-putting-it-all-together-main/lib/shoe.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    
-    def __init__(self, brand, size):
+    def __init__(self, brand):
         self.brand = brand
-        self.size = size
+        self.color = None
+        self.size = None
+        self.material = None
+        self.condition = None
 
     @property
     def size(self):
         return self._size
-    
+
     @size.setter
-    def size(self, size):
-        if type(size) == int:
-            self._size = size
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
         else:
             print("size must be an integer")
 
     def cobble(self):
-        self.condition = "New"
         print("Your shoe is as good as new!")
+        self.condition = "New"
